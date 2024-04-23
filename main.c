689ca41dcc7318e1e6b07d5af90ca5bd6f6a8893
Umut Bide <umut.bide.564@gmail.com>
2024-04-23 16:22:02 +0300

    deneme devam

--- main.c
+++ main.c
@@ -1,2 +1,42 @@
 //deneme github
 // bağlılık kontrolleri 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+  int a;
+  int b;
+  int c;
+} Point;
+
+Point *rotate(Point *p, int angle) {
+  int temp;
+  if (angle % 90 != 0) {
+    return NULL;
+  }
+  angle /= 90;
+  while (angle--) {
+    temp = p->a;
+    p->a = p->b;
+    p->b = -p->c;
+    p->c = temp;
+  }
+  return p;
+}
+
+int main() {
+  Point point = {1, 2, 3};
+  Point *rotated = rotate(&point, 180);
+
+  if (rotated != NULL) {
+    printf("Döndürülmüş nokta: (%d, %d, %d)\n", rotated->a, rotated->b, rotated->c);
+  } else {
+    printf("Nokta döndürülemedi.\n");
+  }
+
+  free(rotated); // Belleği serbest bırakmayı unutmayın!
+
+  return 0;
+}
